9adc5b557071d4573e863fe9e4f980172c4fb52f
matin <matin03@gmail.com>
2017-11-23 13:52:19 +0900

    master 에 올리기

--- matin.sample.webapp-0.0.1/src/main/java/matin/sample/webapp/App.java
+++ matin.sample.webapp-0.0.1/src/main/java/matin/sample/webapp/App.java
@@ -4,10 +4,12 @@ package matin.sample.webapp;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        System.out.println("뭐? 왜? 왜?");
     }
 }
