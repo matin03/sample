dd4b64152674b86082b8bb6b3c58fec01395e25e
matin <matin03@gmail.com>
2017-11-23 13:33:36 +0900

    얍얍

--- java_consoleProject/src/study/lambda/Optional_연습.java
+++ java_consoleProject/src/study/lambda/Optional_연습.java
@@ -5,7 +5,7 @@ public class Optional_연습 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Optional 람다 연습을 시작하지");
+		System.out.println("Optional 람다 연습을 시작하지 엽엽");
 
 	}
 
